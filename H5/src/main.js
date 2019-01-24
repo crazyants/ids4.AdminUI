93a1b7756c1baff518be7d78d15daf4f25124905
zhouyalong <15515533605@163.com>
2019-01-24 14:40:43 +0800

    tab 滚动条

--- H5/src/main.js
+++ H5/src/main.js
@@ -6,6 +6,7 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/reset.css'
+import './assets/index.css'
 import http from './plugin/http.js'
 Vue.config.productionTip = false
 
