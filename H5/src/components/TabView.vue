198f6cdb11dae83a5cfd4f268b3d85161e85391b
owen <964128467@qq.com>
2019-01-24 17:32:34 +0800

    .

--- H5/src/components/TabView.vue
+++ H5/src/components/TabView.vue
@@ -1,40 +1,43 @@
 <template>
-  <el-container>
-    <el-header height="auto">
-      <div>{{CurTabIndex}}</div> 
-      <span v-for="(tab,index) in Tabs" class="el-tag" :class="{'active':CurTabIndex==index}" @click="ActiveTab(index)" :key="tab.path">{{tab.title}}<span v-if="tab.closable" @click="DelTab(index,$event)" class="el-icon-close"></span></span>
-    </el-header>
-    <el-main>
-      <keep-alive>
-        <router-view/>
-      </keep-alive>
-    </el-main>
-  </el-container>
+  <div>
+    <span
+      v-for="(tab,index) in Tabs"
+      class="el-tag"
+      :class="{'active':CurTabIndex==index}"
+      @click="ActiveTab(index)"
+      :key="tab.path"
+    >
+      {{tab.title}}
+      <span v-if="tab.closable" @click="DelTab(index,$event)" class="el-icon-close"></span>
+    </span>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+  </div>
 </template>
 
 <script>
-import { mapState,mapMutations } from 'vuex'
+import { mapState, mapMutations } from "vuex";
 export default {
   name: "tab-view",
-   data() {
-      return {
-        CurTabName: '2',
-        tabIndex: 2
-      }
-    },
-    computed:{
-      ...mapState('tab',['CurTabIndex','Tabs'])
-    },
-    methods: {
-      ...mapMutations('tab',['ActiveTab']),
-      DelTab(index,e) {
-        e.stopPropagation();
-        this.$store.commit('tab/DelTab',index)
-      }
+  data() {
+    return {
+      CurTabName: "2",
+      tabIndex: 2
+    };
+  },
+  computed: {
+    ...mapState("tab", ["CurTabIndex", "Tabs"])
+  },
+  methods: {
+    ...mapMutations("tab", ["ActiveTab"]),
+    DelTab(index, e) {
+      e.stopPropagation();
+      this.$store.commit("tab/DelTab", index);
     }
+  }
 };
 </script>
 
 <style>
-
 </style>
