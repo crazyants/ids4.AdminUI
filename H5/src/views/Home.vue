21ccfea03b61acebfe17c4457d5b49397b5c6368
zhouyalong <15515533605@163.com>
2019-01-29 14:26:03 +0800

    菜单

--- H5/src/views/Home.vue
+++ H5/src/views/Home.vue
@@ -21,72 +21,29 @@
                         <el-submenu index="1">
                             <template slot="title">
                                 <i class="el-icon-location"></i>
-                                <span slot="title">导航一</span>
+                                <span slot="title">人员管理</span>
                             </template>
                             <el-menu-item index="1-1-1">选项1</el-menu-item>
                             <el-menu-item index="1-1-2">选项2</el-menu-item>
                             <el-menu-item index="1-1-3">选项3</el-menu-item>
-                            <el-menu-item index="1-1-4">选项4</el-menu-item>
-                            <el-menu-item index="1-1-5">选项5</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-7">选项7</el-menu-item>
                         </el-submenu>
-                        <el-menu-item index="2">
-                            <i class="el-icon-menu"></i>
-                            <span slot="title">导航二</span>
-                        </el-menu-item>
-                        <el-menu-item index="3" disabled>
-                            <i class="el-icon-document"></i>
-                            <span slot="title">导航三</span>
-                        </el-menu-item>
-                        <el-menu-item index="4">
-                            <i class="el-icon-setting"></i>
-                            <span slot="title">导航四</span>
-                        </el-menu-item>
-
-                        <el-submenu index="1-4">
+                        <el-submenu index="2">
                             <template slot="title">
-                                <i class="el-icon-location"></i>
-                                <span slot="title">导航无</span>
+                                <i class="el-icon-menu"></i>
+                                <span slot="title">系统管理</span>
                             </template>
                             <el-menu-item index="1-1-1">选项1</el-menu-item>
                             <el-menu-item index="1-1-2">选项2</el-menu-item>
                             <el-menu-item index="1-1-3">选项3</el-menu-item>
-                            <el-menu-item index="1-1-4">选项4</el-menu-item>
-                            <el-menu-item index="1-1-5">选项5</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-6">选项6</el-menu-item>
-                            <el-menu-item index="1-1-7">选项7</el-menu-item>
+                        </el-submenu>
+                        <el-submenu index="3">
+                            <template slot="title">
+                                <i class="el-icon-document"></i>
+                                <span slot="title">权限管理</span>
+                            </template>
+                            <el-menu-item index="1-1-1">选项1</el-menu-item>
+                            <el-menu-item index="1-1-2">选项2</el-menu-item>
+                            <el-menu-item index="1-1-3">选项3</el-menu-item>
                         </el-submenu>
                     </el-menu>
                 </el-aside>
