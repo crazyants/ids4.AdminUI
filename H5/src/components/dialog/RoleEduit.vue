2ae3b5e366b14f452018b5afc255a3291f1c05ca
zhouyalong <15515533605@163.com>
2019-02-14 16:45:33 +0800

    表单验证

--- H5/src/components/dialog/RoleEduit.vue
+++ H5/src/components/dialog/RoleEduit.vue
@@ -1,26 +1,24 @@
 <template>
-    <dialogBar @on-close="closeMyself" v-if="isShow" :dialog-w="dialogW">
-        <div slot="header">标题</div>
+    <dialogBar @on-close="closeMyself" v-if="isShow" :dialog-width="dialogWidth">
+        <div slot="header">{{dialogTittle}}</div>
         <div class="dialog_publish_main" slot="main">
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
-            <p>这里是内容插入到子组件的slot的name为main里面，可以在父组件中添加class定义样式，事件类型等各种操作</p>
+            <el-form ref="roleForm" :model="roleForm" :rules="rules" label-width="80px">
+                <el-form-item label="日期" prop='data'>
+                    <el-col :span="18">
+                        <el-date-picker type="date" placeholder="选择日期" v-model="roleForm.date" style="width: 100%;"></el-date-picker>
+                    </el-col>
+                </el-form-item>
+                <el-form-item label="姓名" prop='name'>
+                    <el-input v-model="roleForm.name"></el-input>
+                </el-form-item>
+                <el-form-item label="地址" prop='desc'>
+                    <el-input type="textarea" v-model="roleForm.desc"></el-input>
+                </el-form-item>
+            </el-form>
         </div>
         <div slot="footer">
-            <el-button type="success" size='mini' @click='closeMyself'>成功按钮</el-button>
-            <el-button type="info" size='mini' @click='closeMyself'>信息按钮</el-button>
+            <el-button type="info" size='mini' @click='closeMyself'>取消</el-button>
+            <el-button type="primary" size='mini' @click='onSubmit("roleForm")'>确认</el-button>
         </div>
 
 
@@ -39,21 +37,55 @@
                 default: false,
                 required:true, //必须
             },
-            dialogW:{
+            dialogWidth:{
                 type: Number,
                 default: 500,
+            },
+            dialogTittle:{
+                type: String
+            },
+            roleInfo:{
+                type:Object
             }
         },
         data () {
             return{
-
+                roleForm:{
+                    date:'',
+                    name:'',
+                    desc:'',
+                },
+                rules:{
+                    name:[
+                        {required: true,message:'请输入活动名字',trigger:'blur'},
+                    ],
+                    date:[
+                        { required:true,message:'请选择日期',trigger:'change'}
+                    ],
+                    desc:[
+                        {required:true,message:"请输入地址",trigger:'blur'}
+                    ]
+                }
             }
+        },
+        created(){
+            this.roleForm = this.roleInfo
         },
         methods: {
             closeMyself() {
+                this.$refs.roleForm.resetFields();
                 this.$emit("close-dailog");
                 //如果需要传参的话，可以在"on-close"后面再加参数，然后在父组件的函数里接收就可以了。
             },
+            onSubmit(roleForm){
+                this.$refs[roleForm].validate( (valid) => {
+                    if(valid){
+                        alert('sbumit!')
+                    }else {
+                        console.log('error')
+                    }
+                })
+            }
         }
     }
 </script>
--- H5/src/views/Tabs/RoleManagement.vue
+++ H5/src/views/Tabs/RoleManagement.vue
@@ -1,89 +1,101 @@
 <template>
     <el-scrollbar wrap-class="scrollbar-wrapper-y">
         <div>
-        <el-row class='role_title'>
-            <el-col :span="2">
-                <div>
-                    <h3 class='role_title_text'>角色管理</h3>
-                </div>
-            </el-col>
-            <el-col :span="8" :offset="14">
-                <div>
-                    <el-button type="success" size="mini" icon='el-icon-circle-plus'>创建角色</el-button>
-                    <el-button type="warning" size="mini" icon="el-icon-edit" @click='EduitRole'>编辑</el-button>
-                    <el-button type="danger" size="mini" icon='el-icon-delete' @click='delRole'>删除</el-button>
-                    <el-button type="primary" size="mini" icon='el-icon-news'>分配权限</el-button>
-                </div>
-            </el-col>
-        </el-row>
-        <el-row class='role_system'>
-            <el-col :span="2">
-                <span class='role_system_select'>选择系统 :</span>
-            </el-col>
-            <el-col :span="3">
-                <el-select size="mini" v-model='systemSelect' placeholder="请选择系统">
-                    <el-option label="区域一" value="shanghai"></el-option>
-                    <el-option label="区域二" value="beijing"></el-option>
-                </el-select>
+            <el-row class='role_title'>
+                <el-col :span="2">
+                    <div>
+                        <h3 class='role_title_text'>角色管理</h3>
+                    </div>
+                </el-col>
+                <el-col :span="8" :offset="14">
+                    <div>
+                        <el-button type="success" size="mini" icon='el-icon-circle-plus' @click='addRole'>创建角色
+                        </el-button>
+                        <!--<el-button type="warning" size="mini" icon="el-icon-edit" @click='eduitRole'>编辑</el-button>-->
+                        <el-button type="danger" size="mini" icon='el-icon-delete' @click='delRole'>删除</el-button>
+                        <el-button type="primary" size="mini" icon='el-icon-news'>分配权限</el-button>
+                    </div>
+                </el-col>
+            </el-row>
+            <el-row class='role_system'>
+                <el-col :span="2">
+                    <span class='role_system_select'>选择系统 :</span>
+                </el-col>
+                <el-col :span="3">
+                    <el-select size="mini" v-model='systemSelect' placeholder="请选择系统">
+                        <el-option label="区域一" value="shanghai"></el-option>
+                        <el-option label="区域二" value="beijing"></el-option>
+                    </el-select>
 
-            </el-col>
-            <el-col :span="4" :offset="14">
-                <el-input
-                        v-model="search"
-                        size="mini"
-                        placeholder="输入关键字搜索"/>
-            </el-col>
-        </el-row>
-        <el-row>
-            <el-col>
-                <el-table
-                        ref="multipleTable"
-                        :data="tableData3.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
-                        tooltip-effect="dark"
-                        border
-                        style="width: 100%"
-                        @selection-change="handleSelectionChange">
-                    <el-table-column
-                            type="selection"
-                            >
-                    </el-table-column>
-                    <el-table-column
-                            label="日期"
-                            >
-                        <template slot-scope="scope">{{ scope.row.date }}</template>
-                    </el-table-column>
-                    <el-table-column
-                            prop="name"
-                            label="姓名"
-                            >
-                    </el-table-column>
-                    <el-table-column
-                            prop="address"
-                            label="地址"
-                            show-overflow-tooltip>
-                    </el-table-column>
-                    <el-table-column
-                            label="操作"
-                            width='100'
-                    >
-                        <template slot-scope="scope">
-                            <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-                            <el-button type="text" size="small">编辑</el-button>
-                        </template>
-                    </el-table-column>
-                </el-table>
-            </el-col>
-        </el-row>
+                </el-col>
+                <el-col :span="4" :offset="14">
+                    <el-input
+                            v-model="search"
+                            size="mini"
+                            placeholder="输入关键字搜索"/>
+                </el-col>
+            </el-row>
+            <el-table
+                    ref="multipleTable"
+                    :data="tableData3.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+                    tooltip-effect="dark"
+                    border
+                    size='mini'
+                    style="width: 100%"
+                    @selection-change="handleSelectionChange">
+                <el-table-column
+                        type="selection"
+                >
+                </el-table-column>
+                <el-table-column
+                        label="日期"
+                >
+                    <template slot-scope="scope">{{ scope.row.date }}</template>
+                </el-table-column>
+                <el-table-column
+                        prop="name"
+                        label="姓名"
+                >
+                </el-table-column>
+                <el-table-column
+                        prop="address"
+                        label="地址"
+                        show-overflow-tooltip>
+                </el-table-column>
+                <el-table-column
+                        label="操作"
+                        width='100'
+                >
+                    <template slot-scope="scope">
+                        <el-button @click="roleCheck(scope.row)" type="text" size="small">查看</el-button>
+                        <el-button type="text" size="small" @click='eduitRole(scope.row)'>编辑</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <el-row class='page_footer'>
+                <el-col :span='11' :offset='13'>
+                    <el-pagination
+                            @size-change="handleSizeChange"
+                            @current-change="handleCurrentChange"
+                            :current-page.sync="currentPage3"
+                            :page-size="100"
+                            layout="prev, pager, next, jumper"
+                            :total="1000">
+                    </el-pagination>
+                </el-col>
+            </el-row>
         </div>
-        <RoleEduit :is-show='show' @close-dailog="RoleEduitHide"></RoleEduit>
+        <RoleEduit :is-show='show' @close-dailog="RoleEduitDilagHide" :dialog-tittle='dialogTittle'
+                   :role-info='roleInfo'></RoleEduit>
     </el-scrollbar>
 </template>
 
 <script>
     import RoleEduit from '../../components/dialog/RoleEduit'
+
     export default {
 
-        components:{
+        components: {
             'RoleEduit': RoleEduit,
         },
         data() {
@@ -121,23 +133,25 @@
                 search: '',
                 systemSelect: '',
                 // 对话框
-                show: false
+                dialogTittle: '',
+                show: false,
+                roleInfo: {},
+                // 分页
+                currentPage3: 5,
             }
         },
-        mounted(){
+        mounted() {
             this.flush();
         },
         methods: {
-            async flush(){
-                 const result = await this.$http.post("/base/api/Role/Query", {"pageIndex": 1,"pageSize": 10});
-                 console.log(result);
+            async flush() {
+                const result = await this.$http.post("/base/api/Role/Query", {"pageIndex": 1, "pageSize": 10});
+                console.log(result);
             },
             handleSelectionChange(val) {
                 this.multipleSelection = val;
             },
-            handleClick(row) {
-                console.log(row);
-            },
+
             delRole() {
                 // if (this.multipleSelection.length < 1) {
                 //     this.$message({
@@ -162,12 +176,28 @@
                     });
                 });
             },
-            EduitRole() {
-                this.show = true
+            roleCheck(row) {
+                console.log(row);
             },
-            RoleEduitHide(){
+            eduitRole(row) {
+                this.show = true
+                this.dialogTittle = '编辑角色'
+                this.roleInfo = row
+            },
+            addRole() {
+                this.show = true
+                this.dialogTittle = '创建角色'
+            },
+            RoleEduitDilagHide() {
+                this.roleInfo = {}
                 this.show = false
-            }
+            },
+            handleSizeChange(val) {
+                console.log(`每页 ${val} 条`);
+            },
+            handleCurrentChange(val) {
+                console.log(`当前页: ${val}`);
+            },
         }
     }
 </script>
@@ -191,5 +221,9 @@
             display: inline-block;
             text-indent: 1.5em;
         }
+    }
+
+    .page_footer {
+        margin-top: 10px;
     }
 </style>
