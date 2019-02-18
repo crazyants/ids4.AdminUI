ed9bba6c0b7b686ee987d5c6bbf07338ecd8071b
owen <964128467@qq.com>
2019-02-18 17:42:23 +0800

    .

--- H5/src/components/TabCtrl.js
+++ H5/src/components/TabCtrl.js
@@ -11,6 +11,7 @@ var defaulttab = {//默认打开页面
 
 Vue.mixin({
   activated() {
+    if(!this.$vnode.parent||!this.$vnode.parent.componentInstance.cache) return;
     var title="新页面";
     if(this.$route.meta&&this.$route.meta.title) title = this.$route.meta.title;
     else{
